bf0d060a915ee903a8cf1ed3da8b8eec98a9a5d3
imranZERO <imran.leave@gmail.com>
2023-09-16 23:44:36 +0600

    added TotalPrice feature

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,13 @@ function App() {
 	const [courseList, setCourseList] = useState([]);
 	const [creditHours, setCreditHours] = useState(0);
 	const [creditHoursRemaining, setcreditHoursRemaining] = useState(20);
+	const [totalPrice, setTotalPrice] = useState(0);
 
 	const handleAddToCourseList = (id, course) => {
 		const newCourseList = [...courseList, course];
 		const newCreditHours = creditHours + course.credit;
 		const newCreditHoursRemaining = creditHoursRemaining - course.credit;
+		const newTotalPrice = totalPrice + course.price;
 
 		if (newCreditHours > 20) {
 			toast.error('Not enough remaining credit');
@@ -23,6 +25,7 @@ function App() {
 			setCourseList(newCourseList);
 			setCreditHours(newCreditHours);
 			setcreditHoursRemaining(newCreditHoursRemaining);
+			setTotalPrice(newTotalPrice);
 		}
 	};
 
@@ -35,7 +38,7 @@ function App() {
 				</h1>
 			</header>
 			<div className="max-w-[1366px] mx-auto px-4 pt-6 pb-20 grid grid-cols-1 lg:grid-cols-4 gap-y-5">
-				<main className="grid grid-cols-1 lg:grid-cols-3 px-4 gap-5 col-span-3">
+				<main className="grid grid-cols-1 lg:grid-cols-3 lg:px-4 gap-5 col-span-3">
 					<Courses handleAddToCourseList={handleAddToCourseList}></Courses>
 				</main>
 				<div className="card w-full h-fit bg-base-100 shadow-md px-4 hover:shadow-xl transition col-span-1 lg:order-last order-first py-6">
@@ -47,6 +50,10 @@ function App() {
 					<div className="divider px-4 my-2"></div>
 					<h4 className="text-lg text-[#1C1B1BCC] px-4 font-medium">
 						Total Credit Hour : {creditHours}
+					</h4>
+					<div className="divider px-4 my-2"></div>
+					<h4 className="text-lg text-[#1C1B1BCC] px-4 font-medium">
+						Total Price : {totalPrice}
 					</h4>
 				</div>
 			</div>
